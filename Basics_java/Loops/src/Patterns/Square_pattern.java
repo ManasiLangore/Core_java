6b394313f03318835f8c490c92dc83a20a928bef
Manasi Langore <manasilangore1125@gmail.com>
2025-09-24 21:43:16 +0530

    init commit

---
+++ Basics_java/Loops/src/Patterns/Square_pattern.java
@@ -0,0 +1,31 @@
+package Patterns;
+
+public class Square_pattern {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        //
+
+        //printing Square patterns => * * * * *
+        //							  * * * * *
+        //							  * * * * *
+        // 							  * * * * *
+        //							  * * * * *
+
+        //method 1
+
+//		for(int i=1; i<=5; i++) {
+//
+//			for(int j=1; j<=5; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("*");
+//		}
+
+
+        //method 2
+        for(int row=1; row<=5; row++) {
+            System.out.println(" * * * * *");
+        }
+    }
+}
