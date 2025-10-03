e140428db060cef9155cd3836d23a87cf2c50e78
Manasi Langore <your-email@example.com>
2025-10-03 21:14:05 +0530

    init commit

--- src/Basics_java/Loops/For/For/for_loop.java
+++ src/Basics_java/Loops/For/For/for_loop.java
@@ -7,13 +7,20 @@ public class for_loop {
 
         //printing a table of given number
 
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter a number : ");
-        int n = in.nextInt();
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Enter a number : ");
+//        int n = in.nextInt();
+//
+//
+//        for(int i=1; i<=10; i++) {
+//            System.out.println( n + " * " + i + " = " + (i*n));
+//        }
 
 
-        for(int i=1; i<=10; i++) {
-            System.out.println( n + " * " + i + " = " + (i*n));
+        int n1=10;
+        int n2=2;
+        if(n1%n2==0){
+            System.out.println("num");
         }
 
 
