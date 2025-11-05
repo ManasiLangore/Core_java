8d7a0a8ef352739b8056ec678f8d62428c83c971
Manasi Langore <manasilangore1125@gmail.com>
2025-11-05 12:02:43 +0530

    init commit

--- src/Array/Duplicate_Move_Pairs/Combination.java
+++ src/Array/Duplicate_Move_Pairs/Combination.java
@@ -7,7 +7,7 @@ public class Combination {
         int size =0;
 
         //remove duplicates
-        for(int i=1;i<a.length; i++){
+        for(int i=0;i<a.length; i++){
             int count =0;
             for(int j=i+1; j<a.length; j++){
                 if(a[i]==a[j]){
